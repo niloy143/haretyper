e6b5eea869ab3c857188abb1f1a6f7e644e3ac03
Niloy Mahmud Apu <none@niloy.org>
2025-03-23 00:28:39 +0600

    wpm, timer, restart, and keyboard hand position guide

--- public/js/main.js
+++ public/js/main.js
@@ -1,82 +1,117 @@
+const CHARS_PER_WORD = 6;
+
 const CLASSES = {
 	TYPED: "typed",
 	MISTYPED: "mistyped",
 };
-const charMap = {
-	Enter: "\n",
-	Space: " ",
+
+const charToHtml = {
+	" ": "&#xA0;",
 };
 
 let typed = "";
-const typingArea = document.querySelector("div#typing-area");
-const typingField = document.querySelector("textarea#typing-field");
+let toType = randomAyah();
+let startedAt = Date.now();
+let statsInterval;
 
-typingField.addEventListener("input", function (e) {
-	typed = e.target.value;
-	checkTypedInput();
+const typingAreaEl = document.querySelector("div#typing-area");
+const typingFieldEl = document.querySelector("textarea#typing-field");
+const wpmEl = document.querySelector("div#display-stats #wpm h1");
+const timerEl = document.querySelector("div#display-stats #timer h1");
+const restartBtn = document.querySelector("div#actions button#restart");
+
+typingFieldEl.addEventListener("input", function () {
+	typed = this.value;
+	render();
 });
 
-function checkTypedInput() {
-	let i = 0;
-	for (const wordEl of [...typingArea.children]) {
-		for (const charEl of [...wordEl.children]) {
-			let char = charEl.textContent;
-			if (char.match(/\s/g)) char = " ";
+restartBtn.addEventListener("click", function () {
+	typed = "";
+	toType = randomAyah();
+	startedAt = Date.now();
+	typingFieldEl.value = "";
+	render();
+});
 
-			charEl.classList.remove(CLASSES.MISTYPED);
-			charEl.classList.remove(CLASSES.TYPED);
-
-			if (i >= typed.length) continue;
-			else if (typed[i++] === char) charEl.classList.add(CLASSES.TYPED);
-			else charEl.classList.add(CLASSES.MISTYPED);
-		}
-	}
-
-	// const correctlyTyped = [...typingArea.children].reduce((sum, word) => {
-	// 	return sum + [...word.children].filter((charEl) => charEl.classList.contains(CLASSES.TYPED)).length;
-	// }, 0);
-
-	// if (correctlyTyped && correctlyTyped === typed.length) {
-	// 	console.log(correctlyTyped, typed.length)
-	// 	renderRandomAyah();
-	// }
+function random(max = 113, min = 0) {
+	return Math.round(Math.random() * (max - min)) + min;
 }
 
 function randomAyah() {
-	const surahIndex = Math.round(Math.random() * (quran.length - 1));
-	const surah = quran[surahIndex];
+	const surah = quran[random()];
+	const ayah = surah?.ayahs?.[random(surah?.ayahs?.length || 0)];
+	return ayah?.text || randomAyah();
+}
 
-	if (surah) {
-		const ayahIndex = Math.round(Math.random() * (surah.ayahs?.length - 1));
-		const ayah = surah.ayahs[ayahIndex];
+function getStats() {
+	const seconds = (Date.now() - startedAt) / 1000;
+	const estimatedWords = typed.length / CHARS_PER_WORD;
+	const wpm = estimatedWords / (seconds / 60);
 
-		if (ayah) return ayah.text;
+	return {
+		seconds: Math.round(seconds) || 0,
+		wpm: Math.round(wpm) || 0,
+	};
+}
+
+function displayingStats() {
+	const { seconds, wpm } = getStats();
+
+	timerEl.innerHTML = seconds;
+	wpmEl.innerHTML = wpm;
+}
+
+function startDisplayingStats() {
+	if (!statsInterval) {
+		statsInterval = setInterval(() => {
+			if (!typed.length) startedAt = Date.now();
+			displayingStats();
+		}, 500);
+	}
+}
+
+function stopDisplayingStats() {
+	clearInterval(statsInterval);
+	statsInterval = undefined;
+}
+
+function startTyping() {
+	startedAt = Date.now();
+	startDisplayingStats();
+}
+
+function completeTyping() {
+	typed = "";
+	toType = randomAyah();
+	startedAt = Date.now();
+	typingFieldEl.value = "";
+
+	stopDisplayingStats();
+	render();
+}
+
+function render() {
+	typingAreaEl.innerHTML = "";
+	const wordSpans = [document.createElement("span")];
+
+	for (let i = 0; i < toType.length; i++) {
+		const char = toType[i];
+		const charSpan = document.createElement("span");
+
+		if (typed[i] === char) charSpan.classList.add(CLASSES.TYPED);
+		else if (typeof typed[i] === "string") charSpan.classList.add(CLASSES.MISTYPED);
+
+		charSpan.innerHTML = charToHtml[char] || char;
+		wordSpans[wordSpans.length - 1].appendChild(charSpan);
+
+		if (char.match(/\s/g)) wordSpans.push(document.createElement("span"));
 	}
 
-	return "";
+	wordSpans.forEach((wordSpan) => typingAreaEl.appendChild(wordSpan));
+
+	if (typed && typed === toType) completeTyping();
+	if (typed.length === 1) startTyping();
+	typingFieldEl.focus();
 }
 
-function renderRandomAyah() {
-	const content = randomAyah();
-	const words = content.split(" ");
-
-	typingArea.innerHTML = words
-		.map((word, i) => {
-			const chars = word
-				.split("")
-				.map((char) => `<span>${char}</span>`)
-				.join("");
-
-			return `
-			<span>
-				${chars + (words[i + 1] ? "<span>&#xA0;</span>" : "")}
-			</span>
-		`;
-		})
-		.join("");
-
-	typingField.value = "";
-	typingField.focus();
-}
-
-renderRandomAyah();
+render();
